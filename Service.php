b919808c3b18af137cc10b52d0d5c1aee924bddb
Daniel M <dev@orbitadigital.com>
2020-06-04 17:32:38 +0200

    Create Service.php

---
+++ Service.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @author Daniel Moreno <dev@orbitadigital.com>
+ */
+
+require_once __DIR__ . '/../../config/config.inc.php';
+require_once __DIR__ . '/../../init.php';
+require_once __DIR__ . '/../base.php';
+
+class Service
+{
+}
