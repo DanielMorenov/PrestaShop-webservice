c6e2a668c7b4f32b9d000e94f923a842f25e8ff3
Daniel M <dev@orbitadigital.com>
2020-06-04 17:32:48 +0200

    createApiAccess

--- Service.php
+++ Service.php
@@ -10,4 +10,29 @@ require_once __DIR__ . '/../base.php';
 
 class Service
 {
+    /**
+     * Create api access
+     *
+     * @param int $key number of the limit to consult
+     * @param int $description number of the limit to consult
+     * @return int $id
+     */
+
+    public static function createApiAccess($description = null, $key = null)
+    {
+        Configuration::updateValue('PS_WEBSERVICE', 1);
+
+        if ($key == null) {
+            $permitedCharacteres = '123456789ABCDEFGHJKLMNPQRSTUVWXYZ123456789';
+            $key = substr(str_shuffle($permitedCharacteres), 0, 32);
+        }
+
+        $apiAccess = new WebserviceKey();
+        $apiAccess->key = $key;
+        $apiAccess->description = $description;
+        $apiAccess->save();
+
+        return $apiAccess->id;
+    }
+
 }
